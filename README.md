dbe1cd700db8a5cdfddd847e17901155bc051745
konstantin krivlenia <krivlenia@gmail.com>
2020-04-19 16:58:05 +0300

    add site publisher

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # remarkjs-typograf
 
 plugin to make your typography better with https://github.com/typograf
+
+Example https://mavrin.github.io/remarkjs-typograf/
---
+++ scripts/publishSite.js
@@ -0,0 +1,29 @@
+const ghpages = require("gh-pages");
+const { resolve } = require("path");
+const resolvePath = (relativePath) => resolve(__dirname, relativePath);
+
+const build = resolvePath("../dist");
+
+async function publish() {
+  return new Promise((resolve, reject) => {
+    ghpages.publish(
+      build,
+      {
+        branch: "gh-pages",
+      },
+      function (err) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      }
+    );
+  });
+}
+
+publish()
+  .then(() => console.log("publish to gh-pages branch success"))
+  .catch((err) => {
+    console.log("could not publish to gh-pages branch", err);
+  });
